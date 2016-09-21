d4cf7e605c3856c6165082b4d12be16c49a4af87
Antonin <frantisek.jukl@gmail.com>
2016-09-22 00:27:48 +0200

    chyba pohled
    upraven popis pohledu chyby

--- pohledy/chyba.php
+++ pohledy/chyba.php
@@ -5,6 +5,6 @@
         margin-right: auto }
     </style>
     <div class="centered" style="width:1000px;">
-        <h1>404 Neco se nekde mozna s necim nebo nekym asi podelalo a&nbsp;to zpusobilo, ze stranka, kterou hledate, neexistuje.</h1>
+        <h1>404 Neco se nekde mozna s necim nebo nekym asi podelalo a&nbsp;to zpusobilo, ze stranka, kterou hledate, neexistuje. Ty prodejcu je na ceste k naprave.</h1>
     </div>
     <img class="centered" src="404.jpg" alt="404">
