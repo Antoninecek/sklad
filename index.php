d5685d1d9dcfc985c316d146f068cf0b7a258537
Antonin <frantisek.jukl@gmail.com>
2016-09-27 10:55:51 +0200

    index bez udaju

--- index.php
+++ index.php
@@ -8,31 +8,55 @@ if ($_SERVER['SERVER_NAME'] == "127.0.0.1") {
     define("PATHBASE", "/sklad_new/sklad/");
     define("PATHEMPTYURL", "sklad_new/sklad/pridej");
     define("PATHERR", "sklad_new/sklad/chyba");
+    $phpver = phpversion();
 } else {
-    define("DATABASE", "skladdbjednodussi");
-    define("DBHOST", "localhost");
-    define("DBUSER", "fannyjukl");
-    define("DBPASS", "frantisek");
-    define("PATHEMPTYURL", "projects/sklad/pridej");
-    define("PATHERR", "projects/sklad/chyba");
+    $phpver = phpversion();
+   
+    define("PATHBASE", "/sklad_new/sklad/");
+    define("PATHEMPTYURL", "sklad_new/sklad/pridej");
+    define("PATHERR", "sklad_new/sklad/chyba");
+    print_r($phpver);
 }
 
-function autoloadFunkce($trida) {
-    if (preg_match("/Kontroler$/", $trida)) {
-        require("kontrolery/" . $trida . ".php");
+function parseVersion($phpver) {
+    $arr = explode('.', $phpver);
+    if (intval($arr[0]) >= 5) {
+        if(intval($arr[1]) >= 5){
+            if(intval($arr[2]) >= 8){
+                return TRUE;
+            } else {
+                return FALSE;
+            }
+        } else {
+            return FALSE;
+        }
     } else {
-        require("modely/" . $trida . ".php");
+        return FALSE;
     }
 }
 
-session_start();
+if (parseVersion($phpver)) {
+
+    function autoloadFunkce($trida) {
+        if (preg_match("/Kontroler$/", $trida)) {
+            require("kontrolery/" . $trida . ".php");
+        } else {
+            require("modely/" . $trida . ".php");
+        }
+    }
+
+    session_start();
 
 
-spl_autoload_register("autoloadFunkce");
+    spl_autoload_register("autoloadFunkce");
 
-Db::pripoj(DBHOST, DBUSER, DBPASS, DATABASE);
+    Db::pripoj(DBHOST, DBUSER, DBPASS, DATABASE);
 
-$smerovac = new SmerovacKontroler();
-$smerovac->zpracuj(array($_SERVER['REQUEST_URI']));
-$smerovac->vypisPohled();
+    $smerovac = new SmerovacKontroler();
+    $smerovac->zpracuj(array($_SERVER['REQUEST_URI']));
+    $smerovac->vypisPohled();
+} else {
+    echo "<br>";
+    echo "must have 5.5.8 or higher";
+}
 ?>
